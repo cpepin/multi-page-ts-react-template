2a64bb1541fcc19475c8bceb79e8329b4b084754
CAMERON PEPIN <Cameron.Pepin01@libertymutual.com>
2019-01-14 10:31:11 -0500

    Treeshaking react-router

--- apps/user/index.tsx
+++ apps/user/index.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+// Pulling in es6 versions of react-router
+// in order to tree-shake
+// @ts-ignore
+import Router from 'react-router-dom/es/BrowserRouter';
+// @ts-ignore
+import Route from 'react-router-dom/es/Route';
 
 import { Details } from './components/Details';
 import { Hello } from './components/Hello';
